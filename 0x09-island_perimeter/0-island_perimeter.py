d71098a31dda882794d7f19b3236b082740ea0c4
Salekmostafa <salekelmostafa@gmail.com>
2025-01-11 15:34:12 +0100

    for isl

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,54 +1,19 @@
-#!/usr/bin/python3
-"""Island Perimeter - ALX Interview"""
-
-
-def check_val(x):
-    """_summary_
-
-    Args:
-        n (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    if (x == 0):
-        return 1
-    return 0
-
-
 def island_perimeter(grid):
-    """_summary_
-
-    Args:
-        grid (_type_): _description_
     """
+    Returns the perimeter of the island described in grid.
+    :param grid: List[List[int]], a grid of integers where 0 represents water and 1 represents land.
+    :return: int, the perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "length must be between 1 an 100"
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Start with 4 sides for each land cell
+                perimeter += 4
 
-    x = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                "grid numbers must be 0 or 1"
-            if grid[i][j] == 1:
-                if i-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i-1][j])
-                if j-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i][j-1])
+                # Check for adjacent land cells to subtract shared edges
+                if row > 0 and
 
-                try:
-                    x += check_val(grid[i+1][j])
-                except IndexError:
-                    x += 1
-                try:
-                    x += check_val(grid[i][j+1])
-                except IndexError:
-                    x += 1
-
-    return x
